ce121b75ae657ee2047d0c3a4d17a268e68344c5
Louie Mancini <louiemancini@Kristins-MacBook-Pro-2.local>
2021-02-23 15:56:18 -0800

    added loading dialog

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -12,6 +12,14 @@ class GridView extends Component {
     constructor (props) {
         super(props);
         this.endOfCardsRef = createRef();
+        // Used as a synchronous means of identifying when the component
+        // is fetching cards.  We use this to short-circuit incrementing
+        // the 'page' state variable in the 'onScrollHandler' function.
+        this.isFetchingCards = false;
+        // Used to alleviate flickering of the 'loading' dialog.  We add
+        // a 700ms timeout after we get the API response so the loading
+        // dialog doesn't appear/disappear too fast.
+        this.loadingTimeout = null;
         this.state = {
             page: 1,
             cards: [],
@@ -41,8 +49,10 @@ class GridView extends Component {
     }
 
     getCards () {
-        this.setState({isLoading: true});
         const { page, cards, searchTerm } = this.state; 
+
+        clearTimeout(this.loadingTimeout);
+        this.setState({isLoading: true});
 
         const params = `?page=${page}&pageSize=${PAGE_SIZE}&name=${searchTerm}`;
         Axios.get(PATH + params).then((res) => {
@@ -51,18 +61,22 @@ class GridView extends Component {
         }).catch((err) => {
             alert("An error occurred.  Please realod the page and try again");
         }).finally(() => {
-            this.setState({isLoading: false});
+            this.loadingTimeout = setTimeout(() => 
+                this.setState({isLoading: false})
+            , 700);
+            this.isFetchingCards = false;
         });
     }
 
     handleScrollEvent (e) {
-        if (!this.endOfCardsRef) {
+        if (!this.endOfCardsRef?.current || this.isFetchingCards) {
             return;
         }
 
         const windowHeight = window.innerHeight;
         const refPosition = this.endOfCardsRef.current.getBoundingClientRect();
         if (refPosition.top < windowHeight) {
+            this.isFetchingCards = true;
             this.setState({page: this.state.page + 1});
         }
     }
@@ -86,9 +100,13 @@ class GridView extends Component {
         }
 
         const cardComponents = cards.map((card) => <Card { ...card } />);
+        const endOfCardsRef = <div id="end-of-cards-ref" 
+                                key="end-of-cards-ref" 
+                                ref={ this.endOfCardsRef }>
+                              </div>;
         return (
             <div id="card-canvas">
-                { cardComponents.concat(<div key="end-of-cards-ref" ref={ this.endOfCardsRef }></div>) }
+                { cardComponents.concat(endOfCardsRef) }
             </div>
         ) 
     }
@@ -101,6 +119,11 @@ class GridView extends Component {
                     placeholder="Search for cards by name..." 
                     onKeyUp={ this.handleSearchInput } />
                 { this.buildCards() }
+                { this.state.isLoading &&
+                    <div id="loading-message">
+                        <span>Loading</span>
+                    </div>
+                }
             </div>
         )
     }
