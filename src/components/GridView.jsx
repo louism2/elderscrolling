e650a5724088dfffc2184fd8e315d0876cc8ca97
Louie Mancini <louiemancini@Kristins-MacBook-Pro-2.local>
2021-02-23 11:52:26 -0800

    created GridView

---
+++ src/components/GridView.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class GridView extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {};
+    }
+
+    getCards () {
+        
+    }
+
+    render () {
+        return (
+            <>
+            </>
+        )
+    }
+
+}
+
+export default GridView;
